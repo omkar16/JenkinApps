67eeecd10a4bdca8419f6ec5628ca9e11c58d174
Omkar Bhagwat <omkarrbhagwat@gmail.com>
2022-08-12 15:54:47 +0530

    Add project files.

---
+++ HelloApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MyCalc;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Calc calc = new Calc();
+            Console.WriteLine(calc.Add(10, 50));
+        }
+    }
+}
---
+++ MyCalc/Calc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyCalc
+{
+    public class Calc
+    {
+        public int Add(int x, int y) {
+            return x + y;
+        }
+        public int Multiply(int x, int y) {
+            return x * y;       
+        }
+    }
+}
